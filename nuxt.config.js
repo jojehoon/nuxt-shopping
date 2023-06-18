e488ccb2429b9b93312f4dcea2c0acfc652a86d7
jhjo <jojecoding@gmail.com>
2023-06-18 22:37:15 +0900

    히스토리 모드

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,10 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
+  router: {
+    mode: `history`,
+  },
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['@/assets/css/reset.css'],
 
